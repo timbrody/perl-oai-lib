692a705bba6c8cc8b1ba20efbb35e22d389ab82c
tdb01r <tdb01r@4fbceca2-c7fb-0310-b843-c3b59ac5d5c7>
2011-06-23 13:43:46 +0000

    * Fixed undef warning in MetadataFormat
    git-svn-id: https://svn.eprints.org/citebase/tools/oai-lib@918 4fbceca2-c7fb-0310-b843-c3b59ac5d5c7

--- lib/HTTP/OAI/MetadataFormat.pm
+++ lib/HTTP/OAI/MetadataFormat.pm
@@ -51,8 +51,11 @@ sub end_element {
 	my ($self,$hash) = @_;
 	$self->SUPER::end_element($hash);
 	my $elem = lc($hash->{LocalName});
-	$hash->{Text} =~ s/^\s+//;
-	$hash->{Text} =~ s/\s+$//;
+	if( defined $hash->{Text} )
+	{
+		$hash->{Text} =~ s/^\s+//;
+		$hash->{Text} =~ s/\s+$//;
+	}
 	if( $elem eq 'metadataprefix' ) {
 		$self->metadataPrefix($hash->{Text});
 	} elsif( $elem eq 'schema' ) {
